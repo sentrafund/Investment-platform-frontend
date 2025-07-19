12f15ac0248e3c662267582db43b2cc0dfd337dd
clement <clemcy9@gmail.com>
2025-07-19 09:28:51 +0100

    set api url to remote url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -6,7 +6,7 @@ import axios from "axios";
 const local_url = "http://127.0.0.1:8000/api";
 const remote_url = "https://sentrafund.onrender.com/api";
 
-const BASE_URL = local_url;
+const BASE_URL = remote_url;
 const CSRF_TOKEN =
   "SCUtad4GLspfPzgjBsLe192kkfp39gvOyMRsuPviRLDzvweeTI1xmCFlNiV0bmN0";
 
