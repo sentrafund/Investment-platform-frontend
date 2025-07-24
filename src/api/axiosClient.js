bfbdd405076bce0af06e3ffd3f076d2b6a4cc4a8
clement <clemcy9@gmail.com>
2025-07-24 17:04:47 +0100

    update: change to render api

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -5,7 +5,7 @@ const remote_aws_url = "http://54.90.254.228:8000/api";
 const remote_render_url = "https://sentrafund.onrender.com:443/api";
 const local_url = "http://127.0.0.1:8000/api";
 
-const BASE_URL = local_url;
+const BASE_URL = remote_render_url;
 
 export const axiosClient = axios.create({
   baseURL: BASE_URL,
