8dd5c188392fc211f39bc7bacc87326a238a7862
clement <clemcy9@gmail.com>
2025-07-27 11:51:05 +0100

    update: set api endpoint to namecheap server

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,8 +4,9 @@ import Cookies from "js-cookie";
 const remote_aws_url = "http://54.90.254.228:8000/api";
 const remote_render_url = "https://sentrafund.onrender.com:443/api";
 const local_url = "http://127.0.0.1:8000/api";
+const namecheap_url = "https://server1.sentrafund.com/api";
 
-const BASE_URL = remote_render_url;
+const BASE_URL = namecheap_url;
 
 export const axiosClient = axios.create({
   baseURL: BASE_URL,
